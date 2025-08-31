6b24974573a4cd6843467a660dfdc231fad64ac8
ImElio <peruginoantonio573@gmail.com>
2025-08-31 13:25:04 +0200

    refactor: clean up comments in draw.hpp for clarity

--- src/draw.hpp
+++ src/draw.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
 
-// Core drawing functions
 void NebulaDrawRect(float x, float y, float w, float h,
     int r, int g, int b, int a);
 
@@ -9,12 +8,10 @@ void NebulaDrawText(float x, float y, float scale,
     const std::string& text,
     int r, int g, int b, int a);
 
-// Font options
 void SetTextFont(int font);
 void SetTextRightJustified();
 void SetTextCentered(bool centered);
 
-// Enhanced drawing functions
 void DrawRect(float x, float y, float w, float h,
     int r, int g, int b, int a);
 
@@ -22,7 +19,6 @@ void DrawText(float x, float y, float scale,
     const std::string& text,
     int r, int g, int b, int a);
 
-// Special UI elements
 void DrawSprite(const char* textureDict, const char* textureName,
     float x, float y, float width, float height, float heading,
     int r, int g, int b, int a);
@@ -31,6 +27,5 @@ void DrawBanner(float x, float y, float w, float h);
 
 void DrawNotification(const std::string& text);
 
-// Sprite management
 void RequestTexture(const char* textureDict);
 bool HasTextureLoaded(const char* textureDict);
